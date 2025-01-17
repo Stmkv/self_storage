501f67f4e6b88cc31ad84e70dd7156907c5de6cf
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-01-17 22:26:45 +0300

    refactor(base): исправил баг с работой js в navbar

--- storage/urls.py
+++ storage/urls.py
@@ -4,9 +4,9 @@ from storage import views
 
 app_name = "storage"
 urlpatterns = [
-    path("", views.index, name="boxes"),
+    path("", views.index, name="index"),
     path("faq/", views.faq, name="faq"),
-    path("boxes/", views.boxes, name="index"),
+    path("boxes/", views.boxes, name="boxes"),
     path("my-rent/", views.my_rent, name="my-rent"),
     path("my-rent-empty/", views.my_rent_empty, name="my-rent-empty"),
 ]
