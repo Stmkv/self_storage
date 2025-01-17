6e036a6a21e3cd9e36b482cf7536c7d7eb277c06
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-01-17 22:00:17 +0300

    feat(base.html): Засунул header с навигационной панелью в base

--- storage/urls.py
+++ storage/urls.py
@@ -1,11 +1,12 @@
+from django.urls import path
 
 from storage import views
-from django.urls import path
+
 app_name = "storage"
 urlpatterns = [
-    path("", views.boxes, name="boxes"),
+    path("", views.index, name="boxes"),
     path("faq/", views.faq, name="faq"),
-    path("index/", views.index, name="index"),
+    path("boxes/", views.boxes, name="index"),
     path("my-rent/", views.my_rent, name="my-rent"),
     path("my-rent-empty/", views.my_rent_empty, name="my-rent-empty"),
 ]
