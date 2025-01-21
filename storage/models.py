36aa42a8a2c65506bedadf1a8f1fed0e856a29b9
Roman <frideriko26_93@mail.ru>
2025-01-21 23:57:35 +0300

    созданы модели для Ангаров и Боксов

--- storage/models.py
+++ storage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Warehouse(models.Model):
     address = models.CharField(max_length=100, unique=True)
     total_area = models.FloatField(help_text='Общая площадь ангара в квадратных метрах')
@@ -7,20 +8,24 @@ class Warehouse(models.Model):
     description = models.TextField(blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Ангар'
+        verbose_name_plural = 'Ангары'
+
     def __str__ (self):
         return f'Ангар по адресу {self.address}'
 
+
 class Box(models.Model):
     TYPES = [
-        ('маленький', 'маленький'),
-        ('стандартный', 'Стандартный'),
-        ('большой', 'Большой'),
+        ('маленький до 3м', 'Маленький до 3м'),
+        ('стандартный от 3м до 10м', 'Стандартный от 3м до 10м'),
+        ('большой от 10м', 'Большой от 10м'),
     ]
 
     warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name='boxes')
     number = models.CharField(max_length=99, unique=True)
-    box_type = models.CharField(max_length=20, choices=TYPES)
-    box_area = models.FloatField(help_text='Общая площадь бокса')
+    box_type = models.CharField(max_length=30, choices=TYPES)
     status = models.CharField(max_length=20,
                               choices=[
                                   ('свободен', 'Свободен'),
@@ -30,6 +35,10 @@ class Box(models.Model):
     price_per_month = models.DecimalField(max_digits=10, decimal_places=2, help_text='Цена в месяц в рублях')
     release_date = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Бокс'
+        verbose_name_plural = 'Боксы'
 
     def __str__(self):
         return f'Бокс #{self.number} ({self.box_type}'
@@ -44,4 +53,7 @@ class Box(models.Model):
 class Order(models.Model):
     pass
 
+
+class User(models.Model):
+    pass
 
