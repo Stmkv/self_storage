22f21e8fa38392cac2acbb970e667e13301d1bd2
Роман <frideriko26_93@mail.ru>
2025-01-22 23:28:41 +0300

    обновлена админка

--- storage/admin.py
+++ storage/admin.py
@@ -5,16 +5,14 @@ from storage.models import Warehouse, Box
 
 @admin.register(Warehouse)
 class WarehouseAdmin(admin.ModelAdmin):
-    list_display = ('address', 'number_of_boxes', 'creation_date')
+    list_display = ('address', 'temperature', 'celling_height', 'total_boxes', 'available_boxes', 'price_per_month', 'creation_date')
+    list_filter = ('temperature', 'celling_height', 'creation_date')
     search_fields = ('address',)
-    list_filter = ('creation_date',)
     ordering = ('-creation_date',)
-
 
 @admin.register(Box)
 class BoxAdmin(admin.ModelAdmin):
-    list_display = ('number', 'box_type', 'status', 'price_per_month', 'warehouse')
-    list_filter = ('status', 'box_type', 'warehouse')
-    search_fields = ('number',)
-    ordering = ('number',)
-    readonly_fields = ('number',)
+    list_display = ('warehouse', 'number', 'floor', 'price', 'size', 'dimensions', 'is_available', 'release_date')
+    list_filter = ('warehouse', 'floor', 'is_available', 'release_date')
+    search_fields = ('number', 'warehouse__address')
+    ordering = ('warehouse', 'number')
