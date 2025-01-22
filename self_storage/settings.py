a03f9ab16903390fe01b32a3327cc963b0191028
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-01-22 20:52:26 +0300

    feat(debug_toolbar)

--- self_storage/settings.py
+++ self_storage/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "storage.apps.StorageConfig",
     "users.apps.UsersConfig",
+    "debug_toolbar",
 ]
 AUTH_USER_MODEL = "users.CustomUser"
 LOGIN_REDIRECT_URL = "/my-rent/"
@@ -34,6 +35,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "self_storage.urls"
