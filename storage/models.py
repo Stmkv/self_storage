a948dd61324ceb4e7d1ad73a9488ca33e2613b87
Роман <frideriko26_93@mail.ru>
2025-01-22 23:26:13 +0300

    добавлены floor, price, size, dimensions, is_available и verbose_name в Боксы

--- storage/models.py
+++ storage/models.py
@@ -4,12 +4,12 @@ from users.models import CustomUser
 
 
 class Warehouse(models.Model):
-    address = models.CharField(max_length=100, unique=True)
+    address = models.CharField(max_length=100, unique=True, verbose_name="Адрес склада")
     temperature = models.IntegerField(verbose_name="Температура на складе")
     celling_height = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Высота потолка")
     total_boxes = models.PositiveIntegerField(default=0, verbose_name="Всего боксов")
     available_boxes = models.PositiveIntegerField(verbose_name="Доступные боксы")
-    creation_date = models.DateTimeField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     price_per_month = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за месяц от")
 
     class Meta:
@@ -21,37 +21,25 @@ class Warehouse(models.Model):
 
 
 class Box(models.Model):
-    TYPES = [
-        ("маленький до 3м", "Маленький до 3м"),
-        ("стандартный от 3м до 10м", "Стандартный от 3м до 10м"),
-        ("большой от 10м", "Большой от 10м"),
-    ]
-
     warehouse = models.ForeignKey(
-        Warehouse, on_delete=models.CASCADE, related_name="boxes"
+        Warehouse, on_delete=models.CASCADE, related_name="boxes", verbose_name="Склады"
     )
-    number = models.CharField(max_length=99, unique=True)
-    box_type = models.CharField(max_length=30, choices=TYPES)
-    status = models.CharField(
-        max_length=20,
-        choices=[
-            ("свободен", "Свободен"),
-            ("занят", "Занят"),
-        ],
-        default="свободен",
+    number = models.CharField(max_length=99, unique=True, verbose_name="Номер бокса")
+    floor = models.IntegerField(verbose_name="Этаж")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="Цена"
     )
-    price_per_month = models.DecimalField(
-        max_digits=10, decimal_places=2, help_text="Цена в месяц в рублях"
-    )
-    release_date = models.DateField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True, verbose_name="Дата создания")
+    size = models.CharField(max_length=50, verbose_name="Размер бокса")
+    dimensions = models.CharField(max_length=100, verbose_name="Габариты")
+    is_available = models.BooleanField(default=True, verbose_name="Доступность")
 
     class Meta:
         verbose_name = "Бокс"
         verbose_name_plural = "Боксы"
 
     def __str__(self):
-        return f"Бокс #{self.number} ({self.box_type}"
+        return f"{self.warehouse.name} - {self.number}"
 
     def save(self, *args, **kwargs):
         if not self.number:
